b81272d20b68d8bf625a73fa7923d67162597236
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2020-03-28 15:18:25 +0200

    Initial Commit

---
+++ Variations Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Variations Demo
+//
+//  Created by Ivailo Alexandrov on 28.03.20.
+//  Copyright © 2020 Ivailo Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
